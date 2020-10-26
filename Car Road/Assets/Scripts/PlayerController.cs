6e06e6b08773e34651eb9ce7613339c7f0a9e881
LordSapling <55261860+LordSapling@users.noreply.github.com>
2020-10-26 16:07:35 -0500

    End-ish Game Fix-ups
    Added: Color, win text, counter, collectables, and rotating platforms

--- Car Road/Assets/Scripts/PlayerController.cs
+++ Car Road/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,17 +9,24 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    private int count;
 
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public Text countText;
+    public Text winText;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -43,5 +51,28 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         isOnGround = true;
+    }
+
+    /* Helped me create the end message and the counter
+    // https://www.youtube.com/watch?v=XtR29MmzuT0
+    // https://www.youtube.com/watch?v=bFSLI2cmYYo
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 63)
+        {
+            winText.text = "You Escaped!";
+        }
     }
 }
---
+++ Car Road/Assets/Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // https://www.youtube.com/watch?v=OwIE3GI8Rdg Thanks to this video I was able to figure out how to add rotation to my platforms
+        transform.Rotate(new Vector3(0f, 0f, speed) * Time.deltaTime);
+    }
+}
